0e00fac958d86ec18845b745f077b5ab384916b9
Fukase-Takamasa <ultrafukase@gmail.com>
2022-07-14 12:37:34 +0200

    LgtmUsers用の独自Model構造体を追加

---
+++ SwiftConcurrencyTest-MVP/Model/LgtmUsersModel.swift
@@ -0,0 +1,14 @@
+//
+//  LgtmUsersModel.swift
+//  SwiftConcurrencyTest-MVP
+//
+//  Created by ウルトラ深瀬 on 14/7/22.
+//
+
+import Foundation
+
+struct LgtmUsersModel {
+    let articleId: String
+    let totalLgtmCount: Int
+    let lgtms: [LGTM]
+}
